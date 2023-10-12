de9c49fed485810b52d6638d764f4e3837a6dd73
Trent029 <twaterman29@gmail.com>
2023-10-12 09:31:14 -0400

    fixed #42

---
+++ README.md
@@ -0,0 +1,1 @@
+TeamLHC_SCRUM
---
+++ UI.java
@@ -0,0 +1,16 @@
+package TeamLHC_SCRUM;
+
+import java.util.Scanner;
+
+public class UI {
+    private Scanner scanner;
+    private ProjectFACADE facade;
+
+    public void run() {
+
+    }
+
+    public void displayMainMenu() {
+
+    }
+}
