bae81328d188d5c831cd2861566303f122bcce34
Brendon15 <bdm8@email.sc.edu>
2023-10-12 11:21:49 -0400

    fixed #10

---
+++ User.java
@@ -0,0 +1,31 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String emailID;
+    private String password;
+    private Role role;
+
+    protected void login() {
+
+    }
+
+    protected void logout() {
+
+    }
+
+    public void addTask() {
+
+    }
+
+    public void addProject() {
+
+    }
+
+    public void addComment() {
+
+    }
+
+    public void addDocument() {
+        
+    }
+}
