9cdb84d09fba65ba38a78a7c495f251997ac040b
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-10-14 10:46:35 -0400

    fixed #13

---
+++ DataLoader.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class DataLoader extends DataConstants{
+	
+	public static ArrayList<User> getUsers(){
+		return new ArrayList<User>();
+	}
+
+    public static ArrayList<Project> getProjects(){
+		return new ArrayList<Project>();
+	}
+
+}
