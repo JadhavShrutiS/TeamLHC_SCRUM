1a8ed597b19d971d2f24732404834f77c13b7986
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-10-14 11:07:26 -0400

    fixed #14

---
+++ DataWriter.java
@@ -0,0 +1,13 @@
+public class DataWriter extends DataConstants{
+
+    public static boolean saveUsers()
+    {
+        return true;
+    }
+
+    public static boolean saveProjects()
+    {
+        return true;
+    }
+    
+}
