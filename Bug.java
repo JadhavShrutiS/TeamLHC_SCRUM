7c003d691764ee7134e282a117f48f04a579f4e4
snandury12 <112019093+snandury12@users.noreply.github.com>
2023-10-15 17:32:03 -0400

    Add files via upload

---
+++ Bug.java
@@ -0,0 +1,16 @@
+/**
+ * @author Sri Nandury
+ */
+
+ import java.util.ArrayList;
+
+public class Bug extends Task {
+    
+    public String bug;
+    public User tester;
+    public ArrayList<String> reproductiveSteps;
+
+    Bug(){
+        
+    }
+}
---
+++ Column.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * @author Sri Nandury
+ */
+
+ public class Column {
+
+    public ArrayList<Task> taskList;
+    public String name;
+
+    Column(){
+
+    }
+
+    public void addTask(Task task){
+
+    }
+
+    public void deleteTask(Task task){
+
+    }
+ }
