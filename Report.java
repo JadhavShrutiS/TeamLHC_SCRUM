2b6258189ae66b02c2fc7b5fd8f84b10ac2df002
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-10-16 09:59:23 -0400

    fixed #7

--- Report.java
+++ Report.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 /**
  * @author Yug Desai
  */
 public class Report {
-    public date Date;
+    public LocalDate Date;
     public String time;
     public String projectName;
     public ArrayList<Task> taks;
     public User supervisor;
     public ArrayList<String> links;
-    public Status status;
     public void report() {
         
     }
