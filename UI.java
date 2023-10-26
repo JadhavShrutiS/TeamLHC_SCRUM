f3b5a3c64a00f3cab220f349e4e68522d1895610
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-10-26 12:23:43 -0400

    progress

--- UI.java
+++ UI.java
@@ -11,22 +11,17 @@ public class UI {
         app.logout();
         app.login("bsmith@gmail.com", "12345");
         User currentUser = app.getUser();
-
-        
         System.out.println("Logged in as: " + currentUser);
-        app.addTask("Hehe","Hehe","1");
-        
-        Task currentTask = app.getTask("Hehe");
-        
-        System.out.println("Task "+ currentTask);
-        
-        System.out.println(app.getTasks());
 
+        //Task Related Checks
+        app.addTask("Hehe","Hehe","1");
+        Task currentTask = app.getTask("Hehe");
+        System.out.println("Task "+ currentTask);
+        System.out.println(app.getTasks());
         app.addTask("Trial", "Again", "3");
         System.out.println(app.getTask("Trial"));
-
         System.out.println(app.getTasks());
-        app.logout();
+        app.logout();//to save the tasks to json file
     }
 
     public void displayMainMenu() {
