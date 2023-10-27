0b34e0fca2d44fc650b86798e7ad8225d836ba6f
Trent029 <twaterman29@gmail.com>
2023-10-27 14:22:48 -0400

    bug and feature

--- Bug.java
+++ Bug.java
@@ -2,37 +2,45 @@
  * @author Sri Nandury
  */
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Bug extends Task {
     
     public String bug;
     public User tester;
-    public ArrayList<String> reproductiveSteps;
 
     Bug(String name, String description, int priority, String bug){
         super(name, description, priority);
         this.bug = bug;
         this.tester = null;
-        this.reproductiveSteps = null;
     }
 
     Bug(UUID taskID, String taskName, String taskDescription, int taskPriority, String bug) {
         super(taskID, taskName, taskDescription, taskPriority);
         this.bug = bug;
         this.tester = null;
-        this.reproductiveSteps = null;
     }
 
-    Bug(UUID taskUUID,ArrayList<UUID> userIDs,String taskName, String taskDescription, int taskPriority, ArrayList<Comment> taskComments, ArrayList<String> subtasks, String bug, User tester, ArrayList<String> reproductiveSteps) {
+    Bug(UUID taskUUID,ArrayList<UUID> userIDs,String taskName, String taskDescription, int taskPriority, ArrayList<Comment> taskComments, ArrayList<String> subtasks, String bug, User tester) {
         super(taskUUID, userIDs, taskName, taskDescription, taskPriority, taskComments, subtasks);
         this.bug = bug;
         this.tester = tester;
-        this.reproductiveSteps = reproductiveSteps;
     }
 
     public String getBug() {
         return this.bug;
+    }
+
+    public void editBug(String bug) {
+        this.bug = bug;
+    }
+
+    public User getTester() {
+        return tester;
+    }
+
+    public void editTester(User tester) {
+        this.tester = tester;
     }
 }
--- Feature.java
+++ Feature.java
@@ -3,13 +3,32 @@
  */
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Feature extends Task{
     
     public String feature;
-    public ArrayList<String> toDo;
 
-    Feature(){
+    Feature(String name, String description, int priority, String feature){
+        super(name, description, priority);
+        this.feature = feature;
+    }
 
+    Feature(UUID taskID, String taskName, String taskDescription, int taskPriority, String feature) {
+        super(taskID, taskName, taskDescription, taskPriority);
+        this.feature = feature;
+    }
+
+    Feature(UUID taskUUID,ArrayList<UUID> userIDs,String taskName, String taskDescription, int taskPriority, ArrayList<Comment> taskComments, ArrayList<String> subtasks, String feature) {
+        super(taskUUID, userIDs, taskName, taskDescription, taskPriority, taskComments, subtasks);
+        this.feature = feature;
+    }
+
+    public String getFeature() {
+        return this.feature;
+    }
+
+    public void editFeature(String feature) {
+        this.feature = feature;
     }
 }
