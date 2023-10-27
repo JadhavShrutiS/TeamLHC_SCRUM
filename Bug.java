5299cf51ba1547e1f477ff5d7e8f1ed14f46e6ea
Trent029 <twaterman29@gmail.com>
2023-10-27 14:51:52 -0400

    column stuff

--- Bug.java
+++ Bug.java
@@ -10,10 +10,10 @@ public class Bug extends Task {
     public String bug;
     public User tester;
 
-    Bug(String name, String description, int priority, String bug){
+    Bug(String name, String description, int priority, String bug, User tester){
         super(name, description, priority);
         this.bug = bug;
-        this.tester = null;
+        this.tester = tester;
     }
 
     Bug(UUID taskUUID,ArrayList<UUID> userIDs,String taskName, String taskDescription, int taskPriority, ArrayList<Comment> taskComments, ArrayList<String> subtasks, String bug, User tester) {
