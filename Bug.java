a23652070335fb6ebcddbc3cad95f98218f92069
Trent029 <twaterman29@gmail.com>
2023-10-30 18:53:55 -0400

    prettied up code

--- Bug.java
+++ Bug.java
@@ -10,14 +10,14 @@ public class Bug extends Task {
     public String bug;
     public User tester;
 
-    //Existing bugs
+    //New bugs
     Bug(String name, String description, int priority, String bug, User tester){
         super(name, description, priority);
         this.bug = bug;
         this.tester = tester;
     }
 
-    //New bugs
+    //Existing bugs
     Bug(UUID taskUUID,ArrayList<User> users,String taskName, String taskDescription, int taskPriority, ArrayList<Comment> taskComments, ArrayList<String> subtasks, String bug, User tester2) {
         super(taskUUID, users, taskName, taskDescription, taskPriority, taskComments, subtasks);
         this.bug = bug;
