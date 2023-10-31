496cd067d7bb00be43e0963c7e218c3e824bfdf3
Brendon15 <bdm8@email.sc.edu>
2023-10-31 10:27:22 -0400

    Javadoc made

--- User.java
+++ User.java
@@ -1,5 +1,8 @@
 import java.util.UUID;
 
+/**
+ * User information
+ */
 public class User {
     private UUID uuid;
     private String firstName;
@@ -7,7 +10,14 @@ public class User {
     public String userEmail;
     private String userPassword;
     
-    //for existing user
+    /**
+     * Existing users
+     * @param uuid The ID of a user
+     * @param firstName The user's first name
+     * @param lastName The user's last name
+     * @param userEmail The user's email
+     * @param userPassword The user's password
+     */
     public User(UUID uuid, String firstName, String lastName, String userEmail, String userPassword) {
        this.uuid = uuid;
        this.firstName = firstName;
@@ -16,7 +26,13 @@ public class User {
        this.userPassword = userPassword;
     }
 
-    //for new user
+    /**
+     * New users
+     * @param firstName The user's first name
+     * @param lastName The user's last name
+     * @param userEmail The user's email
+     * @param userPassword The user's password
+     */
     public User(String firstName, String lastName, String userEmail, String userPassword) {
     
        this.uuid = UUID.randomUUID();
@@ -26,27 +42,51 @@ public class User {
        this.userPassword = userPassword;
     }
 
+    /**
+     * Enters the information of a user
+     * @return The user's ID, first and last name, email, and password
+     */
     public String toString()
     {
         return this.uuid+" "+this.firstName+" "+this.lastName+" "+this.userEmail+" "+this.userPassword;
     }
 
+    /**
+     * Gets the user's ID
+     * @return The uuid of a user
+     */
     public UUID getId() {
         return this.uuid;
     }
 
+    /**
+     * Gets the user's first name
+     * @return The first name of a user
+     */
     public String getFirstName() {
         return this.firstName;
     }
 
+    /**
+     * Gets the user's last name
+     * @return The last name of a user
+     */
     public String getLastName() {
         return this.lastName;
     }
 
+    /**
+     * Gets the user's email
+     * @return The email of a user
+     */
     public String getEmailID() {
         return this.userEmail;
     }
 
+    /**
+     * Gets the user's password
+     * @return The password of a user
+     */
     public String getPassword() {
         return this.userPassword;
     }
