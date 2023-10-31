a3fb343a910f43b0954bfa3dff9d9885eb5f180f
Brendon15 <bdm8@email.sc.edu>
2023-10-31 10:39:30 -0400

    Javadoc made

--- UserList.java
+++ UserList.java
@@ -1,17 +1,25 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * A list of users
+ */
 public class UserList {
     private ArrayList<User> users;
     private static UserList userList;
 
-    //Create list of users
+    /**
+     * Creates a list of users
+     */
     public UserList() 
     {
         users= DataLoader.getUsers();
     }
 
-    //Get instance of a user list
+    /**
+     * Get instance of a user list
+     * @return A list of users, whether new or existing
+     */
     public static UserList getInstance() 
     {
         if(userList == null){
@@ -20,7 +28,12 @@ public class UserList {
         return userList;
     }
 
-    //Get a user from the list by their email and password
+    /**
+     * Get a user from the list by their email and password
+     * @param userEmail The user's email
+     * @param userPassword The user's password
+     * @return The user with the email and password (if both exist)
+     */
     public User getUser(String userEmail, String userPassword) {
         
         for(int i=0;i<users.size();i++)
@@ -34,6 +47,11 @@ public class UserList {
         return null;
     }
 
+    /**
+     * Add a user
+     * @param emailID The user's email
+     * @return True if there is not already a user with the email
+     */
     public boolean addUser(String emailID)
     {
         users = DataLoader.getUsers();
@@ -47,7 +65,14 @@ public class UserList {
         return true;
     }
 
-    //Add a user to the list
+    /**
+     * Add a user to the list
+     * @param firstName The user's first name
+     * @param lastName The user's last name
+     * @param userEmail The user's email
+     * @param userPassword The user's password
+     * @return True always, and adds a user to the array list if user can be added (addUser String)
+     */
     public boolean add(String firstName, String lastName, String userEmail, String userPassword)
     {
         User user = new User(firstName, lastName, userEmail, userPassword);
@@ -57,18 +82,28 @@ public class UserList {
         }
         return true;
     }
+    /**
+     * Save the users in the data writer
+     */
     public void saveUsers()
     {
         DataWriter.saveUsers();
     }
 
-    //Get all users from the list
+    /**
+     * Get all users from the list
+     * @return An array list of all users
+     */
     public ArrayList<User> getUsers()
     {
         return users;
     }
     
-    //Get a user by their uuid
+    /**
+     * Get a user by their uuid
+     * @param id The uuid of a user
+     * @return The user with the uuid (if it exists)
+     */
     public User getUser(UUID id)
     {
         for(int i=0; i<users.size(); i++)
