4d55ec7e40d23f17fe14778b83e10e0c7ba29940
Sri Nandury <sdmn2498@gmail.com>
2023-11-09 09:37:08 -0500

    test Project

--- ProjectTest.java
+++ ProjectTest.java
@@ -28,8 +28,8 @@ public class ProjectTest {
     }
 
     @Test
-    public void testProjectWithSpaces(){
-        Project proj1 = new Project("      ", "      ");
+    public void testProjectNameWithSpaces(){
+        Project proj1 = new Project("      ", "idk");
         projects.addProject(proj1);
         assertEquals("      ", proj1.getProjectName());
     }
